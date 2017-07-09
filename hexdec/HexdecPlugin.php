d78b1e4680fe5b50fc9337d911929032c5428ef3
Brad Bell <brad@pixelandtonic.com>
2017-07-09 15:42:01 -0700

    Add 2.5 release feed methods.

--- hexdec/HexdecPlugin.php
+++ hexdec/HexdecPlugin.php
@@ -6,24 +6,60 @@ namespace Craft;
  */
 class HexdecPlugin extends BasePlugin
 {
+	/**
+	 * @return string
+	 */
 	public function getName()
 	{
-	    return 'Hexdec';
+		return 'Hexdec';
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getVersion()
 	{
-	    return '1.0';
+		return '1.0';
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getDeveloper()
 	{
-	    return 'Pixel & Tonic';
+		return 'Pixel & Tonic';
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getDeveloperUrl()
 	{
-	    return 'http://pixelandtonic.com';
+		return 'http://pixelandtonic.com';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPluginUrl()
+	{
+		return 'https://github.com/craftcms/hexdec';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDocumentationUrl()
+	{
+		return $this->getPluginUrl().'/blob/v1/README.md';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getReleaseFeedUrl()
+	{
+		return 'https://raw.githubusercontent.com/craftcms/hexdec/v1/releases.json';
 	}
 
 	public function addTwigExtension()
